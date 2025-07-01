5c1a708ac6bd75d1a494da188c59e4ef34dda3fd
okochi-mika <hiyoko_m@icloud.com>
2025-07-01 22:47:02 +0900

    会員側の認証を実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
             $table->string('occupation')->nullable();
             $table->rememberToken();
             $table->timestamps();
-            $table->updated_at();
         });
     }
 
