deb106bd2fe1912bab813ced9e8eaa7a5ee0b9bc
okochi-mika <hiyoko_m@icloud.com>
2025-07-19 19:26:52 +0900

    会員側のトップページの作成

--- tests/Feature/Admin/TermTest.php
+++ tests/Feature/Admin/TermTest.php
@@ -2,19 +2,45 @@
 
 namespace Tests\Feature\Admin;
 
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TermTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+       use RefreshDatabase;
+
+    // 未ログインのユーザーは会員側のトップページにアクセスできる
+    public function test_guest_can_access_home()
     {
-        $response = $this->get('/');
+        $response = $this->get(route('home'));
 
         $response->assertStatus(200);
+    }
+
+    // ログイン済みの一般ユーザーは会員側のトップページにアクセスできる
+    public function test_user_can_access_home()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('home'));
+
+        $response->assertStatus(200);
+    }
+
+    // ログイン済みの管理者は会員側のトップページにアクセスできない
+    public function test_admin_cannot_access_home()
+    {
+        $admin = new Admin();
+        $admin->email = 'admin@example.com';
+        $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
+
+        $response = $this->actingAs($admin, 'admin')->get(route('home'));
+
+        $response->assertRedirect(route('admin.home'));
     }
 }
