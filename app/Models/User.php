882120c80a3262de2c5df124b78e1566d3c01756
okochi-mika <hiyoko_m@icloud.com>
2025-07-21 23:58:35 +0900

    ファイルの追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,7 @@ class User extends Authenticatable
         return $this->hasMany(Review::class);
     }
 
+    public function reservations() {
+        return $this->hasMany(Reservation::class);
+    }
 }
