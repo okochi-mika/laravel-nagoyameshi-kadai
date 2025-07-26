7f7a8fbdae42b208051441faa5eb2a7be6081df9
okochi-mika <hiyoko_m@icloud.com>
2025-07-26 21:13:57 +0900

    HomeController追加とルーティング修正

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use App\Models\Category;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $highly_rated_restaurants = Restaurant::withAvg('reviews', 'score')->orderBy('reviews_avg_score', 'desc')->take(6)->get();
+        $categories = Category::all();
+        $new_restaurants = Restaurant::orderBy('created_at', 'desc')->take(6)->get();
+
+        return view('home', compact('highly_rated_restaurants', 'categories', 'new_restaurants'));
+    }
+
+        public function reviews() {
+        return $this->hasMany(Review::class);
+    }
+
+    public function ratingSortable($query, $direction) {
+        return $query->withAvg('reviews', 'score')->orderBy('reviews_avg_score', $direction);
+    }
+}
