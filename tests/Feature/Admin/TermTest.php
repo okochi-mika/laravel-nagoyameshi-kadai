07f438378558b28f47aac60c7a6b810697dfbe80
okochi-mika <hiyoko_m@icloud.com>
2025-07-27 14:30:18 +0900

    コードの修正

--- tests/Feature/Admin/TermTest.php
+++ tests/Feature/Admin/TermTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Admin;
 
 use App\Models\Admin;
 use App\Models\User;
+use App\Models\Term;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,36 +12,131 @@ use Tests\TestCase;
 
 class TermTest extends TestCase
 {
-       use RefreshDatabase;
+    use RefreshDatabase;
 
-    // 未ログインのユーザーは会員側のトップページにアクセスできる
-    public function test_guest_can_access_home()
+    // 未ログインのユーザーは管理者側の利用規約ページにアクセスできない
+    public function test_guest_cannot_access_admin_terms_index()
     {
-        $response = $this->get(route('home'));
+        $term = Term::factory()->create();
 
-        $response->assertStatus(200);
+        $response = $this->get(route('admin.terms.index'));
+
+        $response->assertRedirect(route('admin.login'));
     }
 
-    // ログイン済みの一般ユーザーは会員側のトップページにアクセスできる
-    public function test_user_can_access_home()
+    // ログイン済みの一般ユーザーは管理者側の利用規約ページにアクセスできない
+    public function test_user_cannot_access_admin_terms_index()
     {
         $user = User::factory()->create();
 
-        $response = $this->actingAs($user)->get(route('home'));
+        $term = Term::factory()->create();
 
-        $response->assertStatus(200);
+        $response = $this->actingAs($user)->get(route('admin.terms.index'));
+
+        $response->assertRedirect(route('admin.login'));
     }
 
-    // ログイン済みの管理者は会員側のトップページにアクセスできない
-    public function test_admin_cannot_access_home()
+    // ログイン済みの管理者は管理者側の利用規約ページにアクセスできる
+    public function test_admin_can_access_admin_terms_index()
     {
         $admin = new Admin();
         $admin->email = 'admin@example.com';
         $admin->password = Hash::make('nagoyameshi');
         $admin->save();
 
-        $response = $this->actingAs($admin, 'admin')->get(route('home'));
+        $term = Term::factory()->create();
 
-        $response->assertRedirect(route('admin.home'));
+        $response = $this->actingAs($admin, 'admin')->get(route('admin.terms.index'));
+
+        $response->assertStatus(200);
+    }
+
+    // 未ログインのユーザーは管理者側の利用規約編集ページにアクセスできない
+    public function test_guest_cannot_access_admin_terms_edit()
+    {
+        $term = Term::factory()->create();
+
+        $response = $this->get(route('admin.terms.edit', $term));
+
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    // ログイン済みの一般ユーザーは管理者側の利用規約編集ページにアクセスできない
+    public function test_user_cannot_access_admin_terms_edit()
+    {
+        $user = User::factory()->create();
+
+        $term = Term::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('admin.terms.edit', $term));
+
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    // ログイン済みの管理者は管理者側の利用規約編集ページにアクセスできる
+    public function test_admin_can_access_admin_terms_edit()
+    {
+        $admin = new Admin();
+        $admin->email = 'admin@example.com';
+        $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
+
+        $term = Term::factory()->create();
+
+        $response = $this->actingAs($admin, 'admin')->get(route('admin.terms.edit', $term));
+
+        $response->assertStatus(200);
+    }
+
+    // 未ログインのユーザーは利用規約を更新できない
+    public function test_guest_cannot_access_admin_terms_update()
+    {
+        $old_term = Term::factory()->create();
+
+        $new_term_data = [
+            'content' => 'テスト更新'
+        ];
+
+        $response = $this->patch(route('admin.terms.update', $old_term), $new_term_data);
+
+        $this->assertDatabaseMissing('terms', $new_term_data);
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    // ログイン済みの一般ユーザーは利用規約を更新できない
+    public function test_user_cannot_access_admin_terms_update()
+    {
+        $user = User::factory()->create();
+
+        $old_term = Term::factory()->create();
+
+        $new_term_data = [
+            'content' => 'テスト更新'
+        ];
+
+        $response = $this->actingAs($user)->patch(route('admin.terms.update', $old_term), $new_term_data);
+
+        $this->assertDatabaseMissing('terms', $new_term_data);
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    // ログイン済みの管理者は利用規約を更新できる
+    public function test_admin_can_access_admin_terms_update()
+    {
+        $admin = new Admin();
+        $admin->email = 'admin@example.com';
+        $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
+
+        $old_term = Term::factory()->create();
+
+        $new_term_data = [
+            'content' => 'テスト更新'
+        ];
+
+        $response = $this->actingAs($admin, 'admin')->patch(route('admin.terms.update', $old_term), $new_term_data);
+
+        $this->assertDatabaseHas('terms', $new_term_data);
+        $response->assertRedirect(route('admin.terms.index'));
     }
 }
